ca437e3973e17d49032f33e24878f13e0f7dde7c
parneetminhas <157835673+parneetminhas@users.noreply.github.com>
2024-02-24 14:32:24 -0500

    Update Student.js

--- mid term/views/Student.js
+++ mid term/views/Student.js
@@ -1,3 +1,31 @@
+// models/Student.js
+
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+  StudentNumber: {
+    type: String,
+    required: true
+  },
+  FirstName: {
+    type: String,
+    required: true
+  },
+  LastName: {
+    type: String,
+    required: true
+  },
+  EmailAddress: {
+    type: String,
+    required: true
+  }
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+module.exports = Student;
+
+
 // routes/students.js
 
 const express = require('express');
